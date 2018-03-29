c45e21c7e9d085665655d66d409385222f4227e7
chenkongming <337142363@qq.com>
2018-03-29 21:39:21 +0800

    路由懒加载在开发环境和生产环境上的使用

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+//开发环境不适用懒加载
+const _import = require('./_import_' + process.env.NODE_ENV);
 
 Vue.use(Router)
-
+const routerList=[
+  { path: '/', component: _import('testRouter/HelloWorld')},
+];
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: routerList
 })
